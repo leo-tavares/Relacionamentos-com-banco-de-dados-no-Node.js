92d8a8f8aa135e7fa2b142ef9acbe3a4dc86c44f
leo-tavares <leonardotavaresdev@gmail.com>
2020-09-06 00:19:37 -0300

    implementação da rota POST /customers
    - Os testes foram feito com um cliente REST (insomnia). Os testes da aplicação dependem de outras partes que serão construidas nos próximos commits

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -6,6 +6,21 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    try {
+      const {
+        body: { name, email },
+      } = request;
+
+      const createCustomer = container.resolve(CreateCustomerService);
+
+      const customers = await createCustomer.execute({
+        name,
+        email,
+      });
+
+      return response.json(customers);
+    } catch (err) {
+      return response.status(400).json({ err: err.message });
+    }
   }
 }
